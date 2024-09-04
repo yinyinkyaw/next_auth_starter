9efd1175031a485f7ab5bfcba4a983fc4be38195
Yin Yin Kyaw <yinyinkyaw13@gmail.com>
2024-09-04 09:14:13 +0630

    [implement] create to do page

--- app/todos/create/page.tsx
+++ app/todos/create/page.tsx
@@ -1,1 +1,89 @@
-export default function CreateNotePage() {}
+"use client";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useForm } from "react-hook-form";
+
+export default function CreateToDoPage() {
+  const form = useForm();
+
+  return (
+    <section className="container mx-auto p-6">
+      <div className="flex justify-between items-center">
+        <h1 className="text-3xl">New To Do</h1>
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/todos">To Dos</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>Create To Do</BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
+
+      <Form {...form}>
+        <form className="mt-4 space-y-4">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="name">Name</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="status"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="status">Status</FormLabel>
+                <Select>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Choose Status" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectItem value="to-do">To Do</SelectItem>
+                      <SelectItem value="doing">Doing</SelectItem>
+                      <SelectItem value="done">Done</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Create</Button>
+        </form>
+      </Form>
+    </section>
+  );
+}
